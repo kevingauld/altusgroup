3a87eb2986fda5b79913d74f67d8d8aa65dd8980
Kevin Gauld <kevin.gauld@loopspin.com>
2021-03-27 15:40:52 -0400

    Initial check-in of module AltusGroup.OrderSystem

---
+++ AltusGroup.OrderSystem.Api/Controllers/OrderController.cs
@@ -0,0 +1,35 @@
+﻿#region Namespace imports
+
+using Microsoft.AspNetCore.Mvc;
+
+#endregion
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AltusGroup.OrderSystem.Api.Controllers
+{
+    [Route("api/order")]
+    [ApiController]
+    public class OrderController : Controller
+    {
+
+        #region ctor
+
+        public OrderController()
+        {
+
+        }
+
+        #endregion
+
+
+        #region Actions - Order
+
+        //TODO: Get
+        //TODO: Post
+        //TODO: Put
+        //TODO: Delete
+
+        #endregion
+    }
+}
